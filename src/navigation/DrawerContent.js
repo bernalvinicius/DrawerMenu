e4f215c03485ad7847a340e44c02ad9276ca7e2b
bernalvinicius <vini_bernal@hotmail.com>
2021-01-04 19:27:47 +0000

    Separated menu in different components

--- src/navigation/DrawerContent.js
+++ src/navigation/DrawerContent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import {DrawerItem, DrawerContentScrollView} from '@react-navigation/drawer';
+import styles from './styles';
 
 export default (props) => {
   const getCurrentRouteName = () => {
@@ -17,132 +18,50 @@ export default (props) => {
       contentContainerStyle={{
         flex: 1,
       }}>
-      <View style={{marginTop: 120}}>
-        <View
-          style={{
-            alignItems: 'center',
-            marginBottom: 30,
-          }}>
-          <Text
-            style={{
-              color: '#fff',
-              fontFamily: 'OpenSans',
-              fontWeight: 'bold',
-              lineHeight: 38,
-              fontSize: 28,
-            }}>
-            Viktor
-          </Text>
+      <View style={styles.viewDrawer}>
+        <View style={styles.viewTitle}>
+          <Text style={styles.title}>Viktor</Text>
         </View>
 
-        <View
-          style={{
-            marginLeft: 10,
-          }}>
-          <View
-            style={{
-              backgroundColor: '#4B2F3F',
-              borderRadius: 15.5,
-              marginBottom: 30,
-              marginHorizontal: 20,
-            }}>
+        <View style={styles.viewContentDrawer}>
+          <View style={styles.contentDrawerItemActive}>
             <DrawerItem
               label="Start"
-              labelStyle={{
-                color: '#FC8074',
-                fontSize: 17,
-                fontFamily: 'OpenSans',
-                fontWeight: '400',
-                width: 100,
-              }}
+              labelStyle={styles.drawerItemLabelActive}
               onPress={() => props.navigation.navigate('Start')}
             />
           </View>
 
-          <View
-            style={{
-              backgroundColor: '#4B2F3F',
-              borderRadius: 15.5,
-              marginBottom: 30,
-              marginHorizontal: 20,
-            }}>
+          <View style={styles.contentDrawerItem}>
             <DrawerItem
               label="Your Cart"
-              labelStyle={{
-                color: '#FC8074',
-                fontSize: 17,
-                fontFamily: 'OpenSans',
-                fontWeight: '400',
-                width: 100,
-              }}
+              labelStyle={styles.drawerItemLabel}
               onPress={() => props.navigation.navigate('Cart')}
             />
           </View>
 
-          <View
-            style={{
-              backgroundColor: '#4B2F3F',
-              borderRadius: 15.5,
-              marginBottom: 30,
-              marginHorizontal: 20,
-            }}>
+          <View style={styles.contentDrawerItem}>
             <DrawerItem
               label="Favorites"
-              labelStyle={{
-                color: '#FC8074',
-                fontSize: 17,
-                fontFamily: 'OpenSans',
-                fontWeight: '400',
-                width: 100,
-              }}
+              labelStyle={styles.drawerItemLabel}
               onPress={() => props.navigation.navigate('Favorites')}
             />
           </View>
 
-          <View
-            style={{
-              backgroundColor: '#4B2F3F',
-              borderRadius: 15.5,
-              marginBottom: 30,
-              marginHorizontal: 20,
-            }}>
+          <View style={styles.contentDrawerItem}>
             <DrawerItem
               label="Your Orders"
-              labelStyle={{
-                color: '#FC8074',
-                fontSize: 17,
-                fontFamily: 'OpenSans',
-                fontWeight: '400',
-                width: 100,
-              }}
+              labelStyle={styles.drawerItemLabel}
               onPress={() => props.navigation.navigate('Orders')}
             />
           </View>
         </View>
 
-        <View
-          style={{
-            borderBottomColor: '#fff',
-            borderBottomWidth: 1,
-            marginVertical: 30,
-            marginLeft: 30,
-            marginRight: 20,
-          }}
-        />
-        <View
-          style={{
-            marginLeft: 30,
-            marginRight: 20,
-          }}>
+        <View style={styles.viewRow} />
+        <View style={styles.viewSignOut}>
           <DrawerItem
             label="Sign Out"
-            labelStyle={{
-              color: '#fff',
-              fontSize: 17,
-              fontFamily: 'OpenSans',
-              fontWeight: '400',
-              width: 100,
-            }}
+            labelStyle={styles.signoutItem}
             onPress={() => alert('Are you sure to logout?')}
           />
         </View>
---
+++ src/navigation/styles.js
@@ -0,0 +1,64 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  viewDrawer: {
+    marginTop: 120,
+  },
+  viewTitle: {
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  title: {
+    color: '#fff',
+    fontFamily: 'OpenSans',
+    fontWeight: 'bold',
+    lineHeight: 38,
+    fontSize: 28,
+  },
+  viewContentDrawer: {
+    marginLeft: 10,
+    width: '100%',
+  },
+  contentDrawerItemActive: {
+    backgroundColor: '#4B2F3F',
+    borderRadius: 15.5,
+    marginHorizontal: 20,
+  },
+  drawerItemLabelActive: {
+    color: '#FC8074',
+    fontSize: 17,
+    fontFamily: 'OpenSans',
+    fontWeight: '400',
+    width: 100,
+  },
+  contentDrawerItem: {
+    marginHorizontal: 20,
+  },
+  drawerItemLabel: {
+    color: '#fff',
+    fontSize: 17,
+    fontFamily: 'OpenSans',
+    fontWeight: '400',
+    width: 100,
+  },
+  viewRow: {
+    borderBottomColor: '#fff',
+    borderBottomWidth: 1,
+    marginVertical: 30,
+    marginLeft: 30,
+    marginRight: 20,
+  },
+  viewSignOut: {
+    marginLeft: 30,
+    marginRight: 20,
+  },
+  signoutItem: {
+    color: '#fff',
+    fontSize: 17,
+    fontFamily: 'OpenSans',
+    fontWeight: '400',
+    width: 100,
+  },
+});
+
+export default styles;
