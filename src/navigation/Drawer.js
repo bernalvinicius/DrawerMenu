8f867e5b4af89ff4de81683c85e9c296d97f0be4
bernalvinicius <vini_bernal@hotmail.com>
2021-01-04 10:23:02 +0000

    Add react-native-reanimated

--- src/navigation/Drawer.js
+++ src/navigation/Drawer.js
@@ -2,17 +2,52 @@ import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import Navigator from './Navigator';
 import DrawerContent from './DrawerContent';
+import Animated from 'react-native-reanimated';
 
 const Drawer = createDrawerNavigator();
 
-export default () => (
-  <Drawer.Navigator
-    initialRouteName="Start"
-    drawerType="slide"
-    overlayColor="transparent"
-    drawerStyle={{width: '50%', backgroundColor: '#1F1B33'}}
-    contentContainerStyle={{flex: 1}}
-    drawerContent={(props) => <DrawerContent {...props} />}>
-    <Drawer.Screen name="Navigator" component={Navigator} />
-  </Drawer.Navigator>
-);
+export default () => {
+  const [progress, setProgress] = React.useState(new Animated.Value(0));
+
+  const borderRadius = Animated.interpolate(progress, {
+    inputRange: [0, 1],
+    outputRange: [0, 50],
+  });
+
+  const rotate = Animated.interpolate(progress, {
+    inputRange: [0, 1],
+    outputRange: ['0deg', '-10deg'],
+  });
+
+  const translateX = Animated.interpolate(progress, {
+    inputRange: [0, 1],
+    outputRange: ['0px', '55px'],
+  });
+
+  const translateY = Animated.interpolate(progress, {
+    inputRange: [0, 1],
+    outputRange: ['0px', '20px'],
+  });
+
+  const screenStyles = {
+    borderRadius,
+    transform: [{rotate}, {translateX}, {translateY}],
+  };
+
+  return (
+    <Drawer.Navigator
+      initialRouteName="Start"
+      drawerType="slide"
+      overlayColor="transparent"
+      drawerStyle={{width: '50%', backgroundColor: '#1F1B33'}}
+      contentContainerStyle={{flex: 1}}
+      drawerContent={(props) => {
+        setProgress(props.progress);
+        return <DrawerContent {...props} />;
+      }}>
+      <Drawer.Screen name="Navigator">
+        {(props) => <Navigator {...props} style={screenStyles} />}
+      </Drawer.Screen>
+    </Drawer.Navigator>
+  );
+};
--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -3,44 +3,47 @@ import React from 'react';
 import {View, Text, Image, TouchableOpacity} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Cart, Favorites, Orders, Start} from '../views';
+import Animated from 'react-native-reanimated';
 
 const Stack = createStackNavigator();
 
-export default ({navigation}) => {
+export default ({navigation, style}) => {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerTransparent: true,
-        headerTitle: null,
-        headerTitleAlign: 'left',
-        headerLeft: () => (
-          <TouchableOpacity onPress={() => navigation.openDrawer()}>
-            <View
-              style={{
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginTop: 60,
-              }}>
-              <Image
-                source={require('../assets/images/menu.png')}
-                style={{height: 30, width: 30, margin: 30}}
-              />
-              <Text
+    <Animated.View style={[{flex: 1, overflow: 'hidden'}, style]}>
+      <Stack.Navigator
+        screenOptions={{
+          headerTransparent: true,
+          headerTitle: null,
+          headerTitleAlign: 'left',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => navigation.openDrawer()}>
+              <View
                 style={{
-                  color: '#9F9FA0',
-                  fontWeight: '700',
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  marginTop: 60,
                 }}>
-                START
-              </Text>
-            </View>
-          </TouchableOpacity>
-        ),
-      }}>
-      <Stack.Screen name="Start" component={Start} />
-      <Stack.Screen name="Cart" component={Cart} />
-      <Stack.Screen name="Favorites" component={Favorites} />
-      <Stack.Screen name="Orders" component={Orders} />
-    </Stack.Navigator>
+                <Image
+                  source={require('../assets/images/menu.png')}
+                  style={{height: 30, width: 30, margin: 30}}
+                />
+                <Text
+                  style={{
+                    color: '#9F9FA0',
+                    fontWeight: '700',
+                  }}>
+                  START
+                </Text>
+              </View>
+            </TouchableOpacity>
+          ),
+        }}>
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="Cart" component={Cart} />
+        <Stack.Screen name="Favorites" component={Favorites} />
+        <Stack.Screen name="Orders" component={Orders} />
+      </Stack.Navigator>
+    </Animated.View>
   );
 };
